fc84d9168a7358bae49ace8f897fd30364df31da
fayeah <411596701@qq.com>
2024-09-21 10:54:43 +0800

    feat: 2024.9.21

---
+++ assets/AboutView-COgmRQ4P.js
@@ -0,0 +1,1 @@
+import{_ as e,a as o,o as s,b as t}from"./index-CNJnqpC6.js";const a={},c={class:"about"},n=t("h1",null,"This is an about page",-1),_=[n];function r(i,d){return s(),o("div",c,_)}const u=e(a,[["render",r]]);export{u as default};
